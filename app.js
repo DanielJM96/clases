6ee32f8da204d6ce50af14a23cb74a870ed35142
DanielJM96 <daniel_juar_mtz96@outlook.es>
2023-05-11 21:44:54 -0600

    clases y funciones

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const resultados = {
+    suma: 0,
+    resta: 0,
+    multiplicacion: 0,
+    division: 0,
+    potencia: 0
+}
+
+let numero1 = 10, numero2 = 14;
+
+
+// llamar la función 
+
+
+operacionSuma (numero1, numero2, resultados);
+operacionResta(numero1, numero2, resultados); 
+operacionMultiplicacion(numero1, numero2, resultados)
+operacionDivision (numero1, numero2, resultados);
+operacionCuadrados(numero1,resultados);
+
+console.log(resultados);
+
