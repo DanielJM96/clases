b72b0962157976b8a9159f4a7ce5aa6dbf7be671
DanielJM96 <daniel_juar_mtz96@outlook.es>
2023-05-11 21:50:36 -0600

    tabulaciones del inche profe

--- utils/operaciones.js
+++ utils/operaciones.js
@@ -2,38 +2,38 @@
 const operacionSuma = (numero1, numero2, objeto )=>{
     objeto.suma = numero1+numero2
     return objeto; 
-    
-    
-    }
-    
-    //RESTA 
-    const operacionResta =(numero1, numero2, objeto)=>{
+
+
+}
+
+//RESTA 
+const operacionResta =(numero1, numero2, objeto)=>{
     objeto.resta = numero1-numero2
     return objeto;
-    }
-    
-    
-    // MULTIPLICACION 
-    const operacionMultiplicacion = (numero1, numero2, objeto)=>{
-        objeto.multiplicacion = numero1*numero2
-        return objeto; 
-    }
-    //DIVISION 
-    const operacionDivision =(numero1, numero2, objeto)=>{
+}
+
+
+// MULTIPLICACION 
+const operacionMultiplicacion = (numero1, numero2, objeto)=>{
+    objeto.multiplicacion = numero1*numero2
+    return objeto; 
+}
+//DIVISION 
+const operacionDivision =(numero1, numero2, objeto)=>{
     objeto.division= numero1/numero2
     return objeto;
-    }
+}
     
     //CUADRADOS 
-    const operacionCuadrados = (operacionSuma, objeto) => {
-        objeto.potencia= operacionSuma*operacionSuma
-        return objeto; 
-    
-    }
+const operacionCuadrados = (operacionSuma, objeto) => {
+    objeto.potencia= operacionSuma*operacionSuma
+    return objeto; 
+
+}
 module.exports= {
- operacionSuma, 
- operacionResta,
-  operacionMultiplicacion, 
-  operacionDivision,
-   operacionCuadrados,   
+    operacionSuma, 
+    operacionResta,
+    operacionMultiplicacion, 
+    operacionDivision,
+    operacionCuadrados,   
 }    
